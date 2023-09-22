64fb13f51168f672c37edb7d7059d08f7bd7606a
Shree Mangalam <sshreemangalam@gmail.com>
2023-09-22 13:07:49 +0530

    minor changes

--- 01React/src/App.jsx
+++ 01React/src/App.jsx
@@ -1,8 +1,10 @@
+import September from "./Test";
 function App() {
   return (
     <>
       <div>
         <h1>Shree Mangalam - 01 React</h1>
+        <September />
       </div>
     </>
   );
---
+++ 01React/src/Test.jsx
@@ -0,0 +1,9 @@
+function September() {
+  return (
+    <div>
+      <h3>This web app was initialized on 8th September</h3>
+    </div>
+  );
+}
+
+export default September;
